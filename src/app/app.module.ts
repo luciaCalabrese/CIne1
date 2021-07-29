eb15c3bb72a542bf6a1d5c57c0f8ba92e0048e4b
lucia816 <73232190+lucia816@users.noreply.github.com>
2021-07-29 20:59:42 +0200

    okode
    Trabajo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule , FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+title = 'pokemoninfo';
+  Peliculas: any = {};
+  cargada = false;
+  titulo: string = " ";
+  mostrar: boolean = false;
+  datos: any = [];
+  datob: any = []
+nombre: string;
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    console.log("servicio corriendo");
+    http.get("http://www.omdbapi.com/?t="+ this.nombre +"&apikey=bfa0c95e").subscribe(resp => {
+      this.Peliculas = resp;
+      this.cargada = true;
+      console.log(resp)
+    });
+  }
 
+  buscar() {
+    this.http.get("http://www.omdbapi.com/?t=" + this.datob.nombre + "&apikey=bfa0c95e").subscribe(resp => {
+      this.Peliculas = resp;
+      this.cargada = true;
+      console.log(resp)
+    });
+  }
+
+
+  guardar() {
+    // se inserta el dato en el arreglo
+    this.datos.push(this.datob);
+
+
+    // se crea un nuevo objeto para almacenar nuevos datos
+    this.datob = {};
+  }
 }
+
